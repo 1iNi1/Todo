fd7a8ecdad43ab384e6b51d39925c3a9f796634b
1iNi1 <nayir94@hotmail.fr>
2020-07-19 06:53:16 +0200

    Updates

---
+++ precache-manifest.9110fa3b3ebfaecbb7f897f7dc95f285.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35877961655cc61ff2bcb2fb1f472151",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "fac7c32b749d8d30b3d8",
+    "url": "/Todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "33e73a4f6dfe7e719de2",
+    "url": "/Todo/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "fac7c32b749d8d30b3d8",
+    "url": "/Todo/static/js/2.7a554218.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.7a554218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e73a4f6dfe7e719de2",
+    "url": "/Todo/static/js/main.d6804b34.chunk.js"
+  },
+  {
+    "revision": "b75d342943a3e704d238",
+    "url": "/Todo/static/js/runtime-main.812114fc.js"
+  }
+]);
